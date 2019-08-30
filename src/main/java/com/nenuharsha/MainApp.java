f23c6ca862a36ae80b89cfcfe49f13b130b2abf7
NenuHarsha <nenuharsha.com>
2019-08-30 12:01:01 +0530

    Modified MainApp.java

--- src/main/java/com/nenuharsha/MainApp.java
+++ src/main/java/com/nenuharsha/MainApp.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApp {
-	
+	String name= "java";
 	public static void main(String[] args) {
 		SpringApplication.run(MainApp.class, args);
 	}
+	
 
 }
